db586a74a78ab2763a94f2f4233055fd6e37aa7b
nishil <charsi@users.noreply.github.com>
2019-04-10 15:44:59 -0700

    typo

--- app.js
+++ app.js
@@ -17,22 +17,22 @@ function multiply(num1, num2){
 
 }
 
-function multiplynum2321(number){
+function multiplynum321(number){
   return multiply(number, 321);
 }
 
-console.log('321*0 calculated :' +multiplynum2321(0), 'should be '+321*0);
-console.log('321*1 calculated :' +multiplynum2321(1), 'should be '+321*1);
-console.log('321*2 calculated :' +multiplynum2321(2), 'should be '+321*2);
-console.log('321*25 calculated :' +multiplynum2321(25), 'should be '+321*25);
-console.log('321*-3 calculated :' +multiplynum2321(-3), 'should be '+321*-3);
-console.log('321*-1 calculated :' +multiplynum2321(-1), 'should be '+321*-1);
+console.log('321*0 calculated :' +multiplynum321(0), 'should be '+321*0);
+console.log('321*1 calculated :' +multiplynum321(1), 'should be '+321*1);
+console.log('321*2 calculated :' +multiplynum321(2), 'should be '+321*2);
+console.log('321*25 calculated :' +multiplynum321(25), 'should be '+321*25);
+console.log('321*-3 calculated :' +multiplynum321(-3), 'should be '+321*-3);
+console.log('321*-1 calculated :' +multiplynum321(-1), 'should be '+321*-1);
 
-assert.equal(multiplynum2321(2), 2*321);
-assert.equal(multiplynum2321(1), 1*321);
-assert.equal(multiplynum2321(0), 0*321);
-assert.equal(multiplynum2321(-1), -1*321);
-assert.equal(multiplynum2321(-2), -2*321);
+assert.equal(multiplynum321(2), 2*321);
+assert.equal(multiplynum321(1), 1*321);
+assert.equal(multiplynum321(0), 0*321);
+assert.equal(multiplynum321(-1), -1*321);
+assert.equal(multiplynum321(-2), -2*321);
 
 assert.equal(multiply(2,4), 2*4);
 assert.equal(multiply(56,37), 56*37);
