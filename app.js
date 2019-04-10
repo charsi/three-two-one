59017088da7fe6e461afac7c41d8bf8303f35f35
nishil <charsi@users.noreply.github.com>
2019-04-10 13:04:58 -0700

    tests

--- app.js
+++ app.js
@@ -1,26 +1,20 @@
-function multiply(number, by){
-  if(number==0 || by==0){
+const assert = require('assert');
+
+function multiply(by, number){   
+  if(number == 0 || by ==0){
     return 0;
   }
 
-  if(by==0){
-    throw 'can\'t divide by zero!';
-  }
+  return (number+(multiply(number, --by)));
 
-  if(by > 0 && number >0){
-    return (number+(multiply(number, --by)));
-  }
-
-  if(by < 0 || number <0){
-    return (number+(multiply(number, --by)))*-1;
-  }
-  
 }
 
 function multiplyBy321(number){
   return multiply(number, 321);
 }
 
-let result = multiplyBy321(2);
-
-console.log(result);
+assert(multiplyBy321(2) == 642);
+assert(multiplyBy321(1) == 321);
+assert(multiplyBy321(0) == 0);
+assert(multiplyBy321(-1) == -321);
+assert(multiplyBy321(-2) == -642);
