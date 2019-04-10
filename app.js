38e7aabd2698798979d5874fc48ec027343d25e3
nishil <charsi@users.noreply.github.com>
2019-04-10 15:28:23 -0700

    handle double negative

--- app.js
+++ app.js
@@ -1,39 +1,48 @@
 const assert = require('assert');
 
-function multiply(number, by){   
-  if(number == 0 || by ==0){
+function multiply(num1, num2){   
+  if(num1 == 0 || num2 ==0){
     return 0;
   }
 
-  if(by<0){
-    return by+(multiply(by,--number));
+  if(num1<0 && num2<0){
+    let pos1 = Math.abs(num1);
+    let pos2 = Math.abs(num2);
+
+    return multiply(pos1,pos2);
+    
+  } 
+
+  if(num2<0){
+    return num2+(multiply(num2,--num1));
   }  
 
-  return number+(multiply(number, --by));
+  return num1+(multiply(num1, --num2));
 
 }
 
-function multiplyBy321(number){
+function multiplynum2321(number){
   return multiply(number, 321);
 }
 
-console.log('321*0 calculated :' +multiplyBy321(0), 'should be '+321*0);
-console.log('321*1 calculated :' +multiplyBy321(1), 'should be '+321*1);
-console.log('321*2 calculated :' +multiplyBy321(2), 'should be '+321*2);
-console.log('321*25 calculated :' +multiplyBy321(25), 'should be '+321*25);
-console.log('321*-3 calculated :' +multiplyBy321(-3), 'should be '+321*-3);
-console.log('321*-1 calculated :' +multiplyBy321(-1), 'should be '+321*-1);
+console.log('321*0 calculated :' +multiplynum2321(0), 'should be '+321*0);
+console.log('321*1 calculated :' +multiplynum2321(1), 'should be '+321*1);
+console.log('321*2 calculated :' +multiplynum2321(2), 'should be '+321*2);
+console.log('321*25 calculated :' +multiplynum2321(25), 'should be '+321*25);
+console.log('321*-3 calculated :' +multiplynum2321(-3), 'should be '+321*-3);
+console.log('321*-1 calculated :' +multiplynum2321(-1), 'should be '+321*-1);
 
-assert.equal(multiplyBy321(2), 2*321);
-assert.equal(multiplyBy321(1), 1*321);
-assert.equal(multiplyBy321(0), 0*321);
-assert.equal(multiplyBy321(-1), -1*321);
-assert.equal(multiplyBy321(-2), -2*321);
+assert.equal(multiplynum2321(2), 2*321);
+assert.equal(multiplynum2321(1), 1*321);
+assert.equal(multiplynum2321(0), 0*321);
+assert.equal(multiplynum2321(-1), -1*321);
+assert.equal(multiplynum2321(-2), -2*321);
 
 assert.equal(multiply(2,4), 2*4);
 assert.equal(multiply(56,37), 56*37);
 assert.equal(multiply(0,37), 0*37);
 assert.equal(multiply(-2,667), -2*667);
 assert.equal(multiply(4,-25), 4*-25);
+assert.equal(multiply(-4,-25), -4*-25);
 
 
