871ba0e14c6b9058687bb38cd011cb2557c884fe
nishil <charsi@users.noreply.github.com>
2019-04-10 12:31:26 -0700

    func

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+function multiply(number, by){
+  if(number==0 || by==0){
+    return 0;
+  }
+
+  if(by > 0){
+    return (number+(multiply(number, --by)));
+  }
+
+  if(by > 0){
+    return (number+(multiply(number, --by)))*-1;
+  }
+  
+}
+
+
 
 let result = multiply(2,321);
 
