5ab11c2595909202d09fdee7e2c071906f77bbd7
nishil <charsi@users.noreply.github.com>
2019-04-10 12:38:53 -0700

    don't divide by zero

--- app.js
+++ app.js
@@ -3,18 +3,24 @@ function multiply(number, by){
     return 0;
   }
 
-  if(by > 0){
+  if(by==0){
+    throw 'can\'t divide by zero!';
+  }
+
+  if(by > 0 && number >0){
     return (number+(multiply(number, --by)));
   }
 
-  if(by > 0){
+  if(by < 0 || number <0){
     return (number+(multiply(number, --by)))*-1;
   }
   
 }
 
+function multiplyBy321(number){
+  return multiply(number, 321);
+}
 
-
-let result = multiply(2,321);
+let result = multiplyBy321(2);
 
 console.log(result);
