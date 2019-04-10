080197b2cd3a597981747af97893b1e02f722d3b
nishil <charsi@users.noreply.github.com>
2019-04-10 15:43:40 -0700

    typo

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function multiply(num1, num2){
   }
 
   if(num1<0 && num2<0){ // forget about -ve if both negative
-    return multiply(Math.abs(num1),th.abs(num2));
+    return multiply(Math.abs(num1),Math.abs(num2));
   } 
 
   if(num2<0){ //swap numbers around if second number negative
